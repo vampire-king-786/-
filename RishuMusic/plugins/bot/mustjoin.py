19c05f058a9316ebe7ce9d7d8d7a6d54a4237d38
Rishu <vishwakarmarishabh366@gmail.com>
2025-01-04 08:17:19 +0530

    Update mustjoin.py

--- RishuMusic/plugins/bot/mustjoin.py
+++ RishuMusic/plugins/bot/mustjoin.py
@@ -1,42 +1,85 @@
-
+import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from RishuMusic import app
 
-#--------------------------
+# Channels to enforce join
+MUST_JOIN_1 = "Ur_Rishu_143"
+MUST_JOIN_2 = "nenobots"
 
-MUST_JOIN = "Ur_Rishu_143"
-#------------------------
+# List of random images
+IMAGES = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
+
+# Updated list of random captions
+CAPTIONS = [
+    "๏ ᴘʟᴇᴀsᴇ ᴊᴏɪɴ {channel_name} ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ ᴜsɪɴɢ ᴛʜᴇ ʙᴏᴛ!",
+    "๏ ɪᴛ sᴇᴇᴍs ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ ᴊᴏɪɴᴇᴅ {channel_name} ʏᴇᴛ. ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴛᴏ ᴘʀᴏᴄᴇᴇᴅ.",
+    "๏ ᴊᴏɪɴ {channel_name} ᴛᴏ ᴜɴʟᴏᴄᴋ ғᴜʟʟ ᴀᴄᴄᴇss ᴛᴏ ᴍʏ ғᴇᴀᴛᴜʀᴇs!",
+    "๏ ᴛᴏ ᴜsᴇ ᴍᴇ, ʏᴏᴜ ᴍᴜsᴛ ᴊᴏɪɴ {channel_name}. ᴛᴀᴘ ᴛʜᴇ ʟɪɴᴋ ʙᴇʟᴏᴡ.",
+    "๏ {channel_name} ᴍᴜsᴛ ʙᴇ ᴊᴏɪɴᴇᴅ ʙᴇғᴏʀᴇ ʏᴏᴜ ᴄᴀɴ ᴄᴏɴᴛɪɴᴜᴇ!",
+    "๏ ᴊᴏɪɴ {channel_name} ғᴏʀ ᴇxᴄʟᴜsɪᴠᴇ ᴄᴏɴᴛᴇɴᴛ ᴀɴᴅ ғᴜᴛᴜʀᴇ ᴜᴘᴅᴀᴛᴇs!",
+    "๏ {channel_name} ɪs ᴀ ʀᴇǫᴜɪʀᴇᴍᴇɴᴛ ᴛᴏ ᴀᴄᴄᴇss ᴛʜɪs ʙᴏᴛ. ᴊᴏɪɴ ɴᴏᴡ!",
+    "๏ ɴᴇᴠᴇʀ ᴍɪss ᴏᴜᴛ! ᴊᴏɪɴ {channel_name} ᴛᴏ ᴇɴᴊᴏʏ ᴛʜᴇ ғᴜʟʟ ᴇxᴘᴇʀɪᴇɴᴄᴇ.",
+    "๏ {channel_name} ᴋᴇᴇᴘs ʏᴏᴜ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴡɪᴛʜ ᴛʜᴇ ʙᴇsᴛ! ᴊᴏɪɴ ɴᴏᴡ ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ.",
+    "๏ ᴛʜɪs ʙᴏᴛ ʀᴇǫᴜɪʀᴇs ᴍᴇᴍʙᴇʀsʜɪᴘ ɪɴ {channel_name}. ᴘʟᴇᴀsᴇ ᴊᴏɪɴ.",
+    "๏ ᴊᴏɪɴ {channel_name} ᴀɴᴅ sᴛᴀʏ ᴜᴘᴅᴀᴛᴇᴅ! ʏᴏᴜ'ʟʟ ɴᴇᴇᴅ ɪᴛ ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ.",
+    "๏ {channel_name} ɪs ᴀ ᴘʟᴀᴄᴇ ғᴏʀ ᴇxᴄʟᴜsɪᴠᴇ ᴄᴏɴᴛᴇɴᴛ. ᴊᴏɪɴ ɴᴏᴡ ᴛᴏ ᴜɴʟᴏᴄᴋ!",
+]
+
+async def send_join_message(msg: Message, link: str, channel_name: str):
+    image = random.choice(IMAGES)
+    caption = random.choice(CAPTIONS).format(channel_name=channel_name)
+    try:
+        await msg.reply_photo(
+            photo=image,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("• ᴊᴏɪɴ •", url=link)]
+                ]
+            )
+        )
+        await msg.stop_propagation()
+    except ChatWriteForbidden:
+        pass
+
 @app.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
+async def must_join_channels(app: Client, msg: Message):
+    if not (MUST_JOIN_1 and MUST_JOIN_2):
         return
+
     try:
         try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await app.get_chat_member(MUST_JOIN_1, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await app.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply_photo(
-                    photo="https://telegra.ph/file/8407ae58f64a4893f8346.jpg", caption=f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [๏sᴜᴘᴘᴏʀᴛ๏]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ ! ",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
-                            ],
-                            [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/nenobots"),
-                            ]
-                        ]
-                    )
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
+            link1 = f"https://t.me/{MUST_JOIN_1}"
+            await send_join_message(msg, link1, "Ur_Rishu_143")
+
+        try:
+            await app.get_chat_member(MUST_JOIN_2, msg.from_user.id)
+        except UserNotParticipant:
+            link2 = f"https://t.me/{MUST_JOIN_2}"
+            await send_join_message(msg, link2, "Nenobots")
+
     except ChatAdminRequired:
-        print(f"๏ ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_ᴊᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
+        print("Please make me admin in the required channels!")
